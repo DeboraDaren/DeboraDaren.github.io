0d3e1527b2c399f9207eee7a872cd2657efa7955
Usuario Toledo <uept15-user@aluno.pr.senac.br>
2022-09-06 21:59:52 -0300

    Teste

--- Login.js
+++ Login.js
@@ -1,1 +1,24 @@
-const labels = document.querySelectorAll(".form-control");
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+    .split("")
+    .map(
+        (latter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${latter}</span>`
+    )
+    .join("");
+});
+
+function changeTheme() {
+    var checkbox = document.getElementById('tema');
+
+    if (checkbox.checked) {
+        var element = document.body;
+        element.className = "dark";
+    }
+    else {
+        var element = document.body;
+        element.className = "light";
+    }
+}
